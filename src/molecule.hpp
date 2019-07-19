fc6a20862f0aad3dd6367c0407823ffa37af7927
Spuriosity1 <alaric.sanders@gmail.com>
2019-07-19 17:57:40 +1000

    Continue writing

--- src/molecule.hpp
+++ src/molecule.hpp
@@ -9,29 +9,42 @@
 #include <Eigen/Dense>
 #include "nuclei.hpp"
 
+#define SQRT_PI 1.7724538509055159
+
+using namespace Eigen;
+
 // R(r) = sum_s=0^M d_s exp(-alpha_s r^2)$$
 typedef struct{
-    double d[GTO_EXPANSION_ORDER];
-    double a[GTO_EXPANSION_ORDER];
+    double d;
+    double a;
+    double n[3];
+} GTO_term_t;
+
+typedef struct{
+    GTO_term_t terms[GTO_EXPANSION_ORDER];
 } GTO_t;
 
 class Molecule : public Nuclei {
 public:
+    Molecule(uint n);
     void construct_GTO();
     void comp_S();
     void comp_F();
 private:
-    void h(GTO_t &k1, GTO_t &k2);
-    void J(GTO_t &k1, GTO_t &k2);
-    void K(GTO_t &k1, GTO_t &k2);
+    void T();
+    void Uen();
+    void J();
+    void K();
 
     void zero_matrices();
 
-    Eigen::MatrixXd F;
-    Eigen::MatrixXd S;
-    Eigen::MatrixXd C;
+    uint N; // number of expansion coefficients, size of basis
 
-    std::vector<GTO_t> GTO;
+    MatrixXd F;
+    MatrixXd S;
+    MatrixXd C;
+
+    std::vector<GTO_t*> GTO; // N long
 
 };
 
